3adc94a0e175cd156e78bb466d13cb3d231f884d
James Watling <james.watling@gmail.com>
2021-05-15 15:10:54 +0100

    removed cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const app = require("express")();
-const cors = require("cors");
-app.use(cors);
+//const cors = require("cors");
+//app.use(cors);
 
 const faunadb = require("faunadb");
 const { SECRET_KEY } = require("./config");
