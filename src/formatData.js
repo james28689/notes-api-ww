507de2d2133f492bed3a287221f6036e58113f33
James Watling <james.watling@gmail.com>
2021-05-22 10:06:13 +0100

    Fixed bug in formatNoteArray

--- src/formatData.js
+++ src/formatData.js
@@ -7,7 +7,7 @@ module.exports.formatNoteArray = (data) =>  {
             userID: data[i].data.userRef.id,
             title: data[i].data.title,
             content: data[i].data.content,
-            date: Date(noteData[i].data.date["@date"])
+            date: Date(data[i].data.date["@date"])
         });
     }
 
