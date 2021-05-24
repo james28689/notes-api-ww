d987fc0408838b36197588b4aa7a3adc510984b5
James Watling <james.watling@gmail.com>
2021-05-24 18:01:34 +0100

    Fixed math.random error

--- src/index.js
+++ src/index.js
@@ -147,7 +147,7 @@ app.post('/auth/google', async (req, res) => {
 
   const data = {
     email: email,
-    username: name + (Math.floor(Math.random * 99)).toString(),
+    username: name + (Math.floor(Math.random() * 99)).toString(),
     name: name,
     dateJoined: q.Date(currentDate.toISOString().substring(0, 10))
   }
