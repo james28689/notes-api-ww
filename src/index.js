ec844b5b6bb8f76f5cb988e88dd119c64f2bed28
James Watling <james.watling@gmail.com>
2021-06-18 11:04:16 +0100

    changed CORS policy to allow PUT

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', process.env.ALLOWED_URL);
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE')
   next();
 });
 
